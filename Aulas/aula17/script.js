fddf72386d012dd872d7f9d0f6d5c3bedd5ba39e
Lucas Brentano <brentano.l@gmail.com>
2021-06-02 20:57:32 -0300

    Test

--- Aulas/aula17/script.js
+++ Aulas/aula17/script.js
@@ -1,2 +1,28 @@
-alert("Hello World")
-alert("Hello Planet")
+// My first two alerts 
+alert("Hello World");
+alert("Hello Planet");
+
+// document.write test
+var text = "<h1>Minha página</h1>";
+document.write(text);
+
+var num1 = 5;
+var num2 = 8;
+
+alert(num1+num2);
+
+// object test
+var person = {
+    name: "Lucas",
+    address: "Cool address"
+}
+
+alert(person.name +" lives in "+ person.address)
+
+alert(person["name"])
+
+/*
+Big comment.
+
+That's nice :)
+*/
