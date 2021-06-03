613b4a81d1c415661a000bb41e158e8c7b008aa3
Lucas Brentano <brentano.l@gmail.com>
2021-06-02 23:11:16 -0300

    Added text

--- TDEs/TDE Aula 17/script.js
+++ TDEs/TDE Aula 17/script.js
@@ -1,3 +1,12 @@
+/* Elabore uma página que contenha os scripts usando as funções prompt e alert que :
+Leia um valor e imprima os resultados: “É maior que 10” ou “Não é maior que 10” ou ainda “É igual a 10”
+Some dois valores lidos e imprima o resultado
+Leia 2 valores e a operação a ser realizada (+, -, * ou /) e imprima o resultado (use um switch)
+Leia um nome e um valor n e imprima o nome n vezes usando o laço for
+Leia um nome, endereço e e-mail, armazene em um array, depois imprima na tela;
+Faça o mesmo procedimento com um objeto
+*/
+
 // First Topic
 var num = prompt("Type an whole number: ")
 
