b2dc9fb5e09d67436f04f778248bfbfb705cc20c
Lucas Brentano <brentano.l@gmail.com>
2021-06-09 20:50:34 -0300

    Js test
    Aula de web

---
+++ Aulas/aula18/script.js
@@ -0,0 +1,13 @@
+
+function hello(){
+    alert("oi");
+}
+
+function clickButton(){
+    
+    var nome = document.getElementById('nome').value;
+    var sobreNome = document.getElementById('sobreNome').value;
+
+    alert("O nome comple é "+ nome + " " + sobreNome)
+
+}
