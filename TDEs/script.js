91d5e2fa1ca2c8f11ec1855e1bb8d591db637916
Lucas Brentano <brentano.l@gmail.com>
2021-06-17 18:37:23 -0300

    Finish TDE 19

--- TDEs/script.js
+++ TDEs/script.js
@@ -1,3 +1,38 @@
 function bmi_calculate(){
+    weight = document.getElementById("bmi_weight").value
+    height = document.getElementById("bmi_height").value / 100 // Diving it by zero so I don't need to ask the height in meters, but instead with cm
+    bmi = (weight/(height*height)).toFixed(2)
+
+    document.getElementById("bmi_ideal").innerHTML = ""
+    document.getElementById("bmi_below").innerHTML = ""
+    document.getElementById("bmi_above").innerHTML = ""
+    document.getElementById("bmi_obese").innerHTML = ""
+
+    if(bmi < 18.5){
+        document.getElementById("bmi_below").innerHTML = bmi
+    }else if(bmi < 25){
+        document.getElementById("bmi_ideal").innerHTML = bmi
+    }else if(bmi < 30){
+        document.getElementById("bmi_above").innerHTML = bmi
+    }else{
+        document.getElementById("bmi_obese").innerHTML = bmi
+    }
+
+    // \_(ツ)_/¯
+    // switch(bmi){
+    //     case (bmi < 18.5):
+    //         document.getElementById("bmi_below").innerHTML = bmi;
+    //         break;
+    //     case (bmi < 25):
+    //         document.getElementById("bmi_ideal").innerHTML = bmi;
+    //         break;
+    //     case (bmi < 30):
+    //         document.getElementById("bmi_above").innerHTML = bmi;
+    //         break;
+    //     case (bmi > 30):
+    //         document.getElementById("bmi_obese").innerHTML = bmi;
+    //         break;
+            
+    // }
 
 }
