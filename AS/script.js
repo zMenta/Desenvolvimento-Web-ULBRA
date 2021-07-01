2afcf4ac94da49e0038095dd7e84f44a9f6eef21
Lucas Brentano <brentano.l@gmail.com>
2021-07-01 12:18:38 -0300

    AS - 0.2
    Created a basic field validation. Can't be empty

---
+++ AS/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    // Error msg if the newsletter input is empty.
+    $("#bt_newsletter").click(function(){
+
+        let content = $("#f_newsletter").val();
+        console.log("Button Pressed");
+
+        if(content == ""){
+            $("#warning").fadeIn(200).html("Error: The field is empty!")
+            $("#warning").delay(4000).fadeOut(300)
+        };
+
+    });
+
+});
